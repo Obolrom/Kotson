f224d121d9f7a959d95cc5ef8af82add8af1d5d3
Obolrom <obolrom@gmail.com>
2021-12-11 17:36:10 +0200

    primitive types

--- src/main/kotlin/JsonTypes.kt
+++ src/main/kotlin/JsonTypes.kt
@@ -6,6 +6,12 @@ class JsonObject : JsonType {
     private val internalFields: MutableList<JsonField<JsonType>> = mutableListOf()
     val fields: List<JsonField<JsonType>> = internalFields
 
+    val Null get() = JsonNull
+
+    override fun toString(): String {
+        return internalFields.joinToString()
+    }
+
     override fun equals(other: Any?): Boolean {
         if (other !is JsonObject) error("Not compatible types JsonObject and ${other?.javaClass}")
         return other.internalFields == internalFields
@@ -23,10 +29,40 @@ class JsonObject : JsonType {
         internalFields.add(JsonField(this, JsonString(jsonString)))
     }
 
+    infix fun String.by(jsonNumber: Number) {
+        internalFields.add(JsonField(this, JsonNumber(jsonNumber)))
+    }
+
+    infix fun String.by(jsonBoolean: Boolean) {
+        internalFields.add(JsonField(this, JsonBoolean(jsonBoolean)))
+    }
+
+    infix fun String.by(jsonNull: JsonNull) {
+        internalFields.add(JsonField(this, JsonNull))
+    }
+
 }
 
 data class JsonString(private val value: String) : JsonType {
     override fun toString() = value
 }
 
-typealias JsonField<T> = Pair<String, T>
+data class JsonNumber(private val value: Number) : JsonType {
+    override fun toString() = value.toString()
+}
+
+data class JsonBoolean(private val value: Boolean) : JsonType {
+    override fun toString() = value.toString()
+}
+
+object JsonNull : JsonType {
+    override fun toString() = "null"
+}
+
+data class JsonField<T>(val field: Pair<String, T>) {
+
+    constructor(name: String, value: T): this(Pair(name, value))
+
+    override fun toString() = "\"${field.first}\": ${field.second}"
+
+}
--- src/test/kotlin/Tester.kt
+++ src/test/kotlin/Tester.kt
@@ -1,6 +1,5 @@
-import org.junit.Before
 import org.junit.Test
-import kotlin.test.BeforeTest
+import kotlin.math.exp
 import kotlin.test.assertEquals
 
 class Tester {
@@ -17,7 +16,7 @@ class Tester {
         val oneFieldJsonObject = obj {
             "field1" by "value1"
         }
-        assertEquals(expectedField, oneFieldJsonObject.fields[0])
+        assertEquals(expectedField, oneFieldJsonObject.fields[0] as JsonField<JsonString>)
     }
 
     @Test
@@ -34,6 +33,54 @@ class Tester {
         }
         println(multipleStringFieldsJson)
         assertEquals(expected, multipleStringFieldsJson)
+    }
+
+    @Test
+    fun someNumberFieldsJson() {
+        val expected = JsonObject().apply {
+            "one".by(JsonNumber(1))
+            "oneLong".by(JsonNumber(1L))
+            "double".by(JsonNumber(3.14))
+            "float".by(JsonNumber(2.24f))
+            "float2".by(JsonNumber(1.0f))
+            "expo".by(JsonNumber(2.99792458e8))
+        }
+        val multipleNumberFieldsJson = obj {
+            "one" by 1
+            "oneLong" by 1L
+            "double" by 3.14
+            "float" by 2.24f
+            "float2" by 1.0f
+            "expo" by 2.99792458e8
+        }
+        println(multipleNumberFieldsJson)
+        assertEquals(expected, multipleNumberFieldsJson)
+    }
+
+    @Test
+    fun booleanFieldsTest() {
+        val expected = JsonObject().apply {
+            "true".by(JsonBoolean(true))
+            "false".by(JsonBoolean(false))
+        }
+        val booleanFieldsJson = obj {
+            "true" by true
+            "false" by false
+        }
+        println(booleanFieldsJson)
+        assertEquals(expected, booleanFieldsJson)
+    }
+
+    @Test
+    fun nullFieldTest() {
+        val expected = JsonObject().apply {
+            "nullObj".by(JsonNull)
+        }
+        val nullFieldJson = obj {
+            "nullObj" by Null
+        }
+        println(nullFieldJson)
+        assertEquals(expected, nullFieldJson)
     }
 
 }
